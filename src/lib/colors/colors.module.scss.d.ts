200e5299e21ef04e51afdb1302652e94040909a3
Jason Liu <jasoneliu03@gmail.com>
2022-12-19 01:14:35 -0500

    Create color palette

---
+++ src/lib/colors/colors.module.scss.d.ts
@@ -0,0 +1,4 @@
+import type { Color } from '$lib/colors/colors';
+
+export const colors: Record<Color, string>;
+export default colors;
---
+++ src/lib/colors/index.ts
@@ -0,0 +1,4 @@
+import colors from '$lib/colors/colors.module.scss';
+import type { Color } from '$lib/colors/colors';
+
+export { colors, Color };
