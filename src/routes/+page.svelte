14bbcab431598d8c31296664098a85dd4f318a3f
Jason Liu <jasoneliu03@gmail.com>
2022-12-19 21:02:55 -0500

    Add mono font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,25 @@
   import { projects } from '$lib/projects';
 </script>
 
-<section class="section">
-  <h2 class="section__header">Projects</h2>
-  <ProjectList {projects} />
-</section>
+<div class="page">
+  <section class="section">
+    <h1>Jason Liu</h1>
+    <h5 class="mono">Software Engineer, Full Stack Developer</h5>
+  </section>
+  <section class="section">
+    <h2 class="section__header">Projects</h2>
+    <ProjectList {projects} />
+  </section>
+</div>
 
 <style lang="scss">
+  .page {
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 2rem;
+    padding: 2rem 0;
+  }
+
   .section {
     padding: 0 2rem;
 
