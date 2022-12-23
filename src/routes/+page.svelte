396c31e5e9e505aed5bb3569a1f725256a75d486
Jason Liu <jasoneliu03@gmail.com>
2022-12-22 23:19:08 -0500

    Add responsiveness and padding to home section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,10 @@
   <Navbar items={navbarItems} />
   <section class="section home" id="home">
     <h1 class="home__name">Jason Liu</h1>
-    <h4 class="home__headline mono">Software Engineer, Full Stack Developer</h4>
+    <h4 class="home__headline mono">
+      <span>Software Engineer,</span>
+      <span>Full Stack Developer</span>
+    </h4>
     <div class="home__about">
       <p>
         Hello! I’m a junior studying Computer Science and Mathematics at the
@@ -40,17 +43,27 @@
   }
 
   .home {
+    align-items: center;
     display: flex;
     flex-flow: column nowrap;
     height: 100vh;
     justify-content: center;
+    padding: 0 2rem;
     text-align: center;
+
+    &__headline {
+      column-gap: 0.75rem;
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: center;
+    }
 
     &__about {
       display: flex;
       flex-flow: column nowrap;
       gap: 0.5rem;
       margin-top: 3rem;
+      max-width: 30rem;
     }
   }
 </style>
