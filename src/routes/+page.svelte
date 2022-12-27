23f9d3ecf093f52a1616d49621a4107b38533fe8
Jason Liu <jasoneliu03@gmail.com>
2022-12-27 14:01:39 -0500

    Add head tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,23 @@
   const navbarItems = ['home', 'projects'];
 </script>
 
+<svelte:head>
+  <title>Jason Liu — Web Developer</title>
+  <meta
+    name="description"
+    content="Portfolio and personal website of Jason Liu, a web developer and student at the University of Maryland."
+  />
+  <meta name="author" content="Jason Liu" />
+  <meta property="og:title" content="Jason Liu — Web Developer" />
+  <meta
+    property="og:description"
+    content="Portfolio and personal website of Jason Liu, a web developer and student at the University of Maryland."
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="" />
+  <meta property="og:url" content="https://www.jasoneliu.com" />
+</svelte:head>
+
 <div class="page">
   <Navbar items={navbarItems} />
   <HomeSection />
