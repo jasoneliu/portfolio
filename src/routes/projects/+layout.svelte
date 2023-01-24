128fe91dce3d7ccbf0b9aaecf751406487f9a06c
Jason Liu <jasoneliu03@gmail.com>
2023-01-24 00:39:54 -0500

    Move toc to separate component

--- src/routes/projects/+layout.svelte
+++ src/routes/projects/+layout.svelte
@@ -1,57 +1,14 @@
 <script lang="ts">
-  import Toc from 'svelte-toc';
-
-  const breakpoint = 992; // $breakpoint-lg
-  let innerWidth: number;
-  $: showToc = innerWidth > breakpoint;
+  import Toc from '$lib/components/Toc.svelte';
 </script>
 
-<!-- Bind screen width -->
-<svelte:window bind:innerWidth />
-
-{#if showToc}
-  <div class="toc">
-    <Toc
-      {breakpoint}
-      title=""
-      headingSelector=":is(h3, h4):not(.toc-exclude)"
-    />
-  </div>
-{/if}
+<Toc />
 
 <main class="project">
   <slot />
 </main>
 
 <style lang="scss">
-  .toc {
-    position: fixed;
-    top: 0;
-    left: 1rem;
-    padding: inherit;
-
-    & :global(nav) {
-      animation-duration: 0s !important; // Disable svelte transition
-    }
-
-    & :global(li) {
-      transition: color 300ms, font-weight 0ms;
-      background-color: transparent;
-      color: $subtext0;
-      font-weight: 400;
-
-      &:hover {
-        color: $text;
-        font-weight: 900;
-      }
-
-      &:is(.active) {
-        color: $text;
-        font-weight: 900;
-      }
-    }
-  }
-
   .project {
     display: block;
     margin-left: 5rem;
