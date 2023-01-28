f42020b8c187e9018b7029b80f87be0093124298
Jason Liu <jasoneliu03@gmail.com>
2023-01-28 01:32:30 -0500

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,13 @@
 
 <svelte:head>
   <meta name="author" content="Jason Liu" />
+
+  <!-- https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs -->
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+  <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+
   <meta property="og:title" content="Jason Liu — Web Developer" />
   <meta
     property="og:description"
