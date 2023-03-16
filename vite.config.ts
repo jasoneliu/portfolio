ed15d87f992f2bc53af695eb1659e4d65f1e28ff
Jason Liu <jasoneliu03@gmail.com>
2023-03-16 04:30:01 -0400

    Convert images to webp

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite';
 import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
+import { imagetools } from 'vite-imagetools';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [{ ...threeMinifier(), enforce: 'pre' }, sveltekit()],
+  plugins: [
+    // Minimize three.js bundle size
+    { ...threeMinifier(), enforce: 'pre' },
+
+    // Convert images to webp
+    imagetools({
+      defaultDirectives: (url) => {
+        const extension = url.pathname.substring(
+          url.pathname.lastIndexOf('.') + 1
+        );
+        if (['jpg', 'png'].includes(extension)) {
+          return new URLSearchParams({
+            format: 'webp',
+          });
+        }
+        return new URLSearchParams();
+      },
+    }),
+
+    sveltekit(),
+  ],
+
   ssr: {
     noExternal: ['three'],
   },
