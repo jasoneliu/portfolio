15f96b9a72adc27802f00cdcd440cc5da80c8a02
Jason Liu <jasoneliu03@gmail.com>
2023-03-23 05:05:00 -0400

    Type LayoutServerLoad for /resume redirect

--- src/routes/resume/+layout.server.ts
+++ src/routes/resume/+layout.server.ts
@@ -1,9 +1,10 @@
 import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
 
 // Redirect /resume to resume link
-export function load() {
+export const load = (() => {
   throw redirect(
     307,
     'https://drive.google.com/file/d/1839WZa3DpYe1a5eQLdNsCkZ9U_Qb7Xe9/view?usp=sharing'
   );
-}
+}) satisfies LayoutServerLoad;
