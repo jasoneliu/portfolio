9c6f3973a7bec47a3b140f96d777006b8d5b90cc
Jason Liu <jasoneliu03@gmail.com>
2023-03-23 04:42:31 -0400

    Move prerender true to +layout.server and  /resume prerender false to +page.server

--- src/routes/resume/+layout.server.ts
+++ src/routes/resume/+layout.server.ts
@@ -7,6 +7,3 @@ export function load() {
     'https://drive.google.com/file/d/1839WZa3DpYe1a5eQLdNsCkZ9U_Qb7Xe9/view?usp=sharing'
   );
 }
-
-// Disable prerender since /resume redirects to an external link
-export const prerender = false;
---
+++ src/routes/resume/+page.server.ts
@@ -0,0 +1,2 @@
+// Disable prerender since /resume redirects to an external link
+export const prerender = false;
