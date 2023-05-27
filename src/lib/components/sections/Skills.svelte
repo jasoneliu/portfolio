165080eeb5cf50d5de9ac4d3ccb1e9b2386dc923
Jason Liu <jasoneliu03@gmail.com>
2023-05-27 00:55:24 -0400

    Style skills section

--- src/lib/components/sections/Skills.svelte
+++ src/lib/components/sections/Skills.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import backendIcon from '$lib/assets/icons/backend.svg';
-  import frontendIcon from '$lib/assets/icons/frontend.svg';
-  import softwareIcon from '$lib/assets/icons/software.svg';
   import InView from '$lib/components/InView.svelte';
   import { skillsAnchor } from '$lib/store';
 
@@ -10,19 +7,16 @@
     {
       title: 'Software Dev',
       body: 'Experienced in working with the entire software development life cycle, including solution design, implementation, testing, and deployment.',
-      icon: softwareIcon,
-      slug: 'software-dev',
+      slug: 'software',
     },
     {
       title: 'Frontend',
       body: 'Passionate about developing elegant and intuitive UIs. Proficient in HTML/CSS/JS and frontend frameworks such as React, Vue, Angular, and Svelte.',
-      icon: frontendIcon,
       slug: 'frontend',
     },
     {
       title: 'Backend',
       body: 'Skilled at building Node.js backends involving SQL and NoSQL databases, REST APIs, and serverless architectures using cloud-computing platforms such as AWS and Google Cloud.',
-      icon: backendIcon,
       slug: 'backend',
     },
   ];
@@ -43,12 +37,16 @@
       <InView delay={skillIndex * 200}>
         <div class="skills__item">
           <div class="skills__heading">
-            <img
+            <div
               class="skills__icon {skill.slug}"
-              src={skill.icon}
-              alt="{skill.title} Icon"
+              style:mask-image={`url('src/lib/assets/icons/${skill.slug}.svg')`}
             />
-            <h3 class="skills__heading-text {skill.slug}">{skill.title}</h3>
+            <h3
+              class="skills__heading-text {skill.slug}"
+              style="--animation-delay-ms: {skillIndex * 200}"
+            >
+              {skill.title}
+            </h3>
           </div>
           <p>
             {skill.body}
@@ -70,7 +68,7 @@
       padding: 2rem 8vw;
 
       @media screen and (max-width: $breakpoint-lg) {
-        grid-template-rows: 1fr 1fr 1fr;
+        grid-template-rows: auto;
         grid-template-columns: 1fr;
       }
     }
@@ -85,37 +83,65 @@
     &__heading-text {
       display: flex;
       flex-direction: column;
+      text-shadow: 0 0.25rem 0.5rem rgba($crust, 0.75);
 
       &::after {
         content: '';
-        opacity: 0.8;
         z-index: -1;
         margin-top: -0.85rem;
-        width: 100%;
+        width: 0%;
         height: 0.5rem;
-        transform: width 1s;
+        animation: 0.7s $ease-out-cubic underlineHeading;
+        animation-delay: calc(0.5s + var(--animation-delay-ms) * 0.001s);
+        animation-fill-mode: forwards;
+
+        @media screen and (max-width: $breakpoint-md) {
+          animation-delay: 0.5s;
+        }
       }
 
-      &.software-dev::after {
-        background-color: darken($red, 10%);
+      &.software::after {
+        background-color: $red;
       }
 
       &.frontend::after {
-        background-color: darken($blue, 10%);
+        background-color: $blue;
       }
 
       &.backend::after {
-        background-color: darken($green, 15%);
+        background-color: $green;
       }
     }
 
     &__icon {
       width: 4rem;
       height: 4rem;
+      mask-position: center;
+      mask-size: contain;
+      mask-repeat: no-repeat;
+
+      &.software {
+        background-image: linear-gradient(135deg, $red, 80%, $yellow);
+      }
 
       &.frontend {
         transform: translate3d(0, -0.125rem, 0);
+        background-image: linear-gradient(135deg, $blue, 65%, $mauve);
       }
+
+      &.backend {
+        background-image: linear-gradient(135deg, $green, 65%, $sapphire);
+      }
+    }
+  }
+
+  @keyframes underlineHeading {
+    0% {
+      width: 0%;
+    }
+
+    100% {
+      width: 100%;
     }
   }
 </style>
